9ca15fdc7bf56807882d9a96ab4f6faed42d1d07
fenic <fenic@DESKTOP-82C9COL>
2019-03-17 11:44:43 +0200

    Course Work with tests

---
+++ RunningString/src/ua/com/qalight/AppRunner.java
@@ -0,0 +1,32 @@
+package ua.com.qalight;
+
+import java.util.List;
+import java.util.Map;
+
+import ua.com.qalight.entity.CurrencyEntity;
+import ua.com.qalight.service.FileManager;
+import ua.com.qalight.util.CurrencyMapper;
+
+public class AppRunner {
+
+	public static void main(String[] args) {
+
+		runWithTimeout(2);
+
+	}
+
+	static void runWithTimeout(int minutes) {
+		while (true) {
+			Map<String, Double> currencyMap = FileManager.readInputCurrencyValues();
+			List<CurrencyEntity> currencyEntities = CurrencyMapper.getFromMap(currencyMap);
+
+			FileManager.writeCurrencyValuesToFile(currencyEntities);
+
+			try {
+				Thread.sleep(1000 * 60 * minutes);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
